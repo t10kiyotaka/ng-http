ea9cb6d95e16597b5bfa2b1f30a8a70d2ac81125
Kiyotaka <t10kiyotaka@gmail.com>
2019-12-03 09:24:48 +0900

    Refactor: Move fetch method to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
 
@@ -17,42 +16,21 @@ export class AppComponent implements OnInit {
               private postsService: PostsService) {}
 
   ngOnInit() {
-    this.onFetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.postsService.createAndFetchPosts(postData);
+    this.postsService.createAndStorePosts(postData);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http.get<{ [key: string]: Post }>(
-      'https://ng-practice2.firebaseio.com//posts.json'
-    )
-    .pipe(
-      map(postData => {
-        const postArray: Post[] = [];
-        for (const key in postData) {
-          if (postData.hasOwnProperty(key)) {
-            postArray.push({...postData[key], id: key});
-          }
-        }
-        return postArray;
-      })
-    )
-    .subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
-  }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({providedIn: 'root'})
@@ -8,7 +9,7 @@ export class PostsService {
 
   constructor(private http: HttpClient) {}
 
-  createAndFetchPosts(postData: Post) {
+  createAndStorePosts(postData: Post) {
     this.http
       .post<{ name: string }>(
         'https://ng-practice2.firebaseio.com//posts.json',
@@ -16,6 +17,25 @@ export class PostsService {
       )
       .subscribe(responseData => {
         console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    this.http.get<{ [key: string]: Post }>(
+      'https://ng-practice2.firebaseio.com//posts.json'
+    )
+      .pipe(
+        map(postData => {
+          const postArray: Post[] = [];
+          for (const key in postData) {
+            if (postData.hasOwnProperty(key)) {
+              postArray.push({...postData[key], id: key});
+            }
+          }
+          return postArray;
+        })
+      )
+      .subscribe(posts => {
       });
   }
 }
